83f8983d1f2680ccfffac3d5036aa17806058ff3
Eduardo0725 <eduardom0142@gmail.com>
2020-03-19 20:02:04 -0300

    PHP do Jeito Certo

---
+++ PHPJeitoCerto/01-ola-mundo-vamos-comecar/01-iniciando-um-projeto/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/style.css" />
+    <title>Vamos começar</title>
+</head>
+
+<body>
+    <?php
+    /**
+    *Bloco PHP com variável start
+    *e comando de saída!
+    */
+    $start = "Vamos começar";
+    echo "<h1>Hello World! {$start}</h1>";
+    echo "<p id='js'>loading...</p>";
+    ?>
+    <script src="assets/scripts.js"></script>
+</body>
+
+</html>
