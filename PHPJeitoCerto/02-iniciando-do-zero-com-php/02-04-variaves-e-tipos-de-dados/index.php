e43421841261b6d955662f965d871fbcef4cb882
Eduardo0725 <eduardom0142@gmail.com>
2020-03-19 23:09:42 -0300

    PHP do Jeito Certo

--- PHPJeitoCerto/02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
+++ PHPJeitoCerto/02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
@@ -8,20 +8,89 @@ fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+$userFirstName = "Eduardo";
+$userLastName = "Carvalho";
+
+echo "<p>{$userFirstName} {$userLastName}</p>";
+
+$user_first_name = $userFirstName;
+$user_last_name = $userLastName;
+
+echo "<p>{$user_first_name} {$user_last_name}</p>";
+
+//variável variável
+$company = "UpInside";
+$$company = "treinamento";
+
+echo "<h1>{$company} {$UpInside}</h1>";
+
+$calcA = 20;
+$calcB = 10;
+
+$calcB = &$calcA;
+
+$calcB = 80;
+
+var_dump([
+    "a" => $calcA,
+    "b" => $calcB
+]);
 
 /**
  * [ tipo boleano ] true | false
  */
 fullStackPHPClassSession("tipo boleano", __LINE__);
 
+$true = true;
+$false = false;
+
+var_dump([
+    $true,
+    $false
+]);
+
+$user_age = 35;
+$best_age = ($user_age > 30);
+var_dump($best_age);
+
+$a = 0;
+$b = 0.0;
+$c = "";
+$d = [];
+$e = null;
+
+if($a || $b || $c || $d || $e){
+    var_dump(true);
+}else{
+    var_dump(false);
+}
 
 /**
  * [ tipo callback ] call | closure
  */
 fullStackPHPClassSession("tipo callback", __LINE__);
 
+$code = "<article><h1>Olá Mundo!</h1></article>";
+$codeClear = call_user_func("strip_tags",$code);
+var_dump($code, $codeClear);
+
+$codeMore = function($code){
+    var_dump($code);
+};
+
+$codeMore("#boraProgramar!");
 
 /**
  * [ outros tipos ] string | array | objeto | numérico | null
  */
 fullStackPHPClassSession("outros tipos", __LINE__);
+
+$string = "Olá Mundo!";
+$array = [$string];
+$object = new stdClass();
+$object->hello = $string;
+$null = null;
+$int = 1234567;
+$float = 1.234567;
+
+var_dump($string, $array, $object, $null, $int, $float);
--- PHPJeitoCerto/02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
+++ PHPJeitoCerto/02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
@@ -8,20 +8,77 @@ echo fullStackPHPClassName("02.05 - Operadores na prática");
  */
 fullStackPHPClassSession("atribuição", __LINE__);
 
+$atrA = 5;
+echo '<br>$atrA += 5 => ', $atrA += 5;
+echo '<br>$atrA -= 5 => ', $atrA -= 5;
+echo '<br>$atrA *= 5 => ', $atrA *= 5;
+echo '<br>$atrA /= 5 => ', $atrA /= 5;
+
+//incremento e descremento
+$incrementA = 5;
+$incrementB = 5;
+
+$increment = [
+    "pós-incremento" => $incrementA++,
+    "res-incremento" => $incrementA,
+    "pré-incremento" => ++$incrementA,
+    "pós-decremento" => $incrementB--,
+    "res-decremento" => $incrementB,
+    "pré-decremento" => --$incrementB
+];
+
+echo "<pre>", var_dump($increment), "</pre>";
 
 /**
  * [ comparação ] https://php.net/manual/pt_BR/language.operators.comparison.php
  */
 fullStackPHPClassSession("comparação", __LINE__);
 
+$relatedA = 5;
+$relatedB = "5";
+$relatedC = 10;
+$related = [
+    "a == b" => ($relatedA == $relatedB),
+    "a === b" => ($relatedA === $relatedB),
+    "a != b" => ($relatedA != $relatedB),
+    "a !== b" => ($relatedA !== $relatedB),
+    "a > c" => ($relatedA > $relatedC),
+    "a < c" => ($relatedA < $relatedC),
+    "a >= b" => ($relatedA >= $relatedB),
+    "a >= c" => ($relatedA >= $relatedC),
+    "a <= c" => ($relatedA <= $relatedC),
 
+];
+echo "<pre>", var_dump($related), "</pre>";
 /**
  * [ lógicos ] https://php.net/manual/pt_BR/language.operators.logical.php
  */
 fullStackPHPClassSession("lógicos", __LINE__);
+
+$logicA = true;
+$logicB = false;
+$logic = [
+    "a && b" => ($logicA && $logicB),
+    "a || b" => ($logicA || $logicB),
+    "a" => ($logicA),
+    "!a" => (!$logicA),
+    "!b" => (!$logicB)
+];
+echo "<pre>", var_dump($logic), "</pre>";
 
 
 /**
  * [ aritiméticos ] https://php.net/manual/pt_BR/language.operators.arithmetic.php
  */
 fullStackPHPClassSession("aritiméticos", __LINE__);
+
+$calcA = 5;
+$calcB = 10;
+$calc = [
+    "a + b" => ($calcA + $calcB),
+    "a - b" => ($calcA - $calcB),
+    "a * b" => ($calcA * $calcB),
+    "a / b" => ($calcA / $calcB),
+    "a % b" => ($calcA % $calcB)
+];
+echo "<pre>", var_dump($calc), "</pre>";
